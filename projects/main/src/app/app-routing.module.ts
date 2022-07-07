d92350c6f27b6ae81a89bd45b216f7ae86032cf3
Your Name <you@example.com>
2022-07-07 15:30:13 +0530

    added main app

--- projects/main/src/app/app-routing.module.ts
+++ projects/main/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {
+    path : '',
+    pathMatch : 'full',
+    component : LoginComponent
+  },
+  {
+    path : 'contacts',
+    pathMatch : 'full',
+    loadChildren : () => import('mfe1/ContactsModule').then((m)=> {
+      return m.ContactsModule
+    })
+  }
 ];
 
 @NgModule({
